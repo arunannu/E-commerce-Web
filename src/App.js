f3bd56f3dbff49c0e839a00da8267068ac4a2f75
Arun <arunkushwahaayan@gmail.com>
2023-12-20 10:08:17 +0530

    ecommerceWeb

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ListItems from "./components/ListItems";
+import { AppContext } from "./context/AppContext";
+
+function App() {
+  return (
+    <>
+      <AppContext>
+        <ListItems />
+      </AppContext>
+    </>
+  );
+}
+
+export default App;
